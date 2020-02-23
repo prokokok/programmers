9ccf16052c1532fc48277a03fd18a203944b86a8
prokokok <philhoon1991@gmail.com>
2020-02-23 10:31:34 +0900

    adding permute function

--- scribbling.py
+++ scribbling.py
@@ -11,5 +11,15 @@ def combinations(numbers):
                 res.append(c + j)
     return res
 
+def permutations(numbers):
+    if len(numbers) < 2:
+        return numbers
+    res = []
 
-po = combinations('1234')
+    for i, c in enumerate(numbers):
+        for cc in permutations(numbers[:i] + numbers[i+1:]):
+            res.append(c +cc)
+    return res
+
+comb = combinations('1234')
+permut = permutations('1234')
