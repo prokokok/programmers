4edaff21e3df7411605c4a4ccc627b961ed02950
prokokok <philhoon1991@gmail.com>
2020-02-26 12:28:20 +0900

    adding script bfs_dfs 여행경로

---
+++ BFS_DFS/여행경로.py
@@ -0,0 +1,26 @@
+def solution(tickets):
+    routes = {}
+
+    for t in tickets:
+        routes[t[0]] = routes.get(t[0],[]) + [t[1]]
+
+    for r in routes:
+        routes[r].sort(reverse=True)
+
+    stack = ["ICN"]
+    path = []
+
+    while len(stack) > 0:
+        top = stack[-1]
+        if top not in routes or len(routes[top]) == 0:
+            path.append(stack.pop())
+        else:
+            stack.append(routes[top][-1])
+            routes[top] = routes[top][:-1]
+
+    return path[::-1]
+
+if __name__ == "__main__":
+    tickets = [['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]
+    tickets = [['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]
+    solution(tickets)
