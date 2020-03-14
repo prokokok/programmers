ab83a4de0d4d2e8336716470e59e75814c475ed6
prokokok <philhoon1991@gmail.com>
2020-03-14 12:23:00 +0900

    adding demis

--- 스택_큐/Lv3_FloodFill.py
+++ 스택_큐/Lv3_FloodFill.py
@@ -51,3 +51,36 @@ def solution(n, m, image):
                 cnt += 1
                 search(image, n, m, col, x=i, y=j)
     return cnt
+
+# Demi's
+
+from collections import deque
+
+def visitable(n, m, x, y, visited):
+    return 0 <= x < n and 0 <= y < m and not visited[x][y]
+
+def bfs(n, m, x, y, image, visited):
+    queue = deque()
+    queue.append((x, y))
+
+    while queue:
+        x, y = queue.popleft()
+        DELTAS = ((0, 1), (0, -1), (1, 0), (-1, 0))
+        for dy, dx in DELTAS:
+            next_x, next_y = x + dx, y + dy
+            if visitable(n, m, next_x, next_y, visited) and image[next_x][next_y] == image[x][y]:
+                queue.append((next_x, next_y))
+                visited[next_x][next_y] = True
+
+
+def solution(n, m, image):
+    answer = 0
+    visited = [[False] * m for _ in range(n)]
+
+    for i in range(n):
+        for j in range(m):
+            if not visited[i][j]:
+                visited[i][j] = True
+                bfs(n, m, i, j, image, visited)
+                answer += 1
+    return answer
