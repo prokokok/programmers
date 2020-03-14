c5ffb62b0e795ff91e7abe2a456745bd28856659
prokokok <philhoon1991@gmail.com>
2020-03-14 12:22:22 +0900

    adding demi answer

--- 힙/Lv3_게임아이템.py
+++ 힙/Lv3_게임아이템.py
@@ -52,3 +52,29 @@ def solution(healths, items):
 
     answer.sort()
     return answer
+
+# Demi's
+from collections import deque
+import heapq
+
+def solution(healths, items):
+    healths.sort()
+    items = [(*item, idx) for idx, item in enumerate(items, 1)]
+
+    BUFF, DEBUFF, INDEX = 0, 1, 2
+    items.sort(key=lambda x:x[DEBUFF])
+    items = deque(items)
+
+    candidates = []
+    answer = []
+
+    for health in healths:
+        while items and health - items[0][DEBUFF] >= 100:
+            item = items.popleft()
+            heapq.heappush(candidates, (-item[BUFF], item[INDEX]))
+
+        if candidates:
+            answer.append(heapq.heappop(candidates)[1])
+
+    answer.sort()
+    return answer
